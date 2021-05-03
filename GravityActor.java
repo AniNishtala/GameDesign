c844e64f76c63f6def7d88fbe6db58f8984a2b94
AnirudhNishtala <71745540+AniNishtala@users.noreply.github.com>
2021-05-03 15:58:56 -0500

    Java files

---
+++ GravityActor.java
@@ -0,0 +1,19 @@
+import mayflower.*;
+public class GravityActor extends Actor
+{
+    boolean landed;
+    public GravityActor()
+    {
+        landed=false;
+    }
+
+     public void act()
+    {
+
+    }
+    
+    // public boolean isBlocked()
+    // {
+        // return this.isTouching(Block.class);
+    // }
+}
